54d5af3b8e4e76e22aa3fa036398c6d76bdabc94
ET01 <EasonTsai0502@gmail.com>
2022-10-12 16:34:11 +0800

    basic

--- basic.cpp
+++ basic.cpp
@@ -1,8 +1,6 @@
 /*
-[#]tree,dsu
-此程式馬可快速查詢每個點和點之間是否連線
-[exQ]https://zerojudge.tw/ShowProblem?problemid=a445
-[AC]
+[Q]
+[]
 */
 
 /*include*/
@@ -31,30 +29,11 @@ using namespace std;
 /*struct*/
 
 /*fn宣告*/
-/*自訂義*/
-const INT maxn=10000;
+
 /*num*/
-INT dsu[maxn];
+
 /*fn定義*/
-void dsu_re(INT n){//初始化
-	for(INT i=0;i<n;i++){
-		dsu[i]=i;
-	}
-}
-INT dsu_find(INT n){//尋找此值的老大
-	if(dsu[n]==n)return n;//如果自己就是老大，回報自己
-	INT root=dsu_find(dsu[n]);//找尋他的老大
-	//dsu[n]=n的爸爸
-	dsu[n]=root;//把老大當自己爸爸
-	return root;//回報自己ㄉ老大
-}
-INT dsu_insert(INT a,INT b){//將a和b連在一起
-	dsu[dsu_find(a)]=dsu_find(b);
-	//讓a的老大歸順b的老大
-}
-bool dsu_get(INT a,INT b){//查詢a和b是否連線
-	return dsu_find(a)==dsu_find(b);
-}
+
 /*main*/
 int main(){
 	{/*IO加速*/
@@ -63,23 +42,6 @@ int main(){
 		ios::sync_with_stdio(false);
 	}
 	/*CIN*/
-	INT n,m,q;
-	//n為人數
-	//m為朋友關係有幾筆
-	//q為問題數
-	cin>>n>>m>>q;
-	dsu_re(n);
-	while(m--){
-		INT a,b;
-		cin>>a>>b;
-		dsu_insert(a,b);
-	}
-	while(q--){
-		INT a,b;
-		cin>>a>>b;
-		if(dsu_get(a,b))cout<<":)\n";
-		else cout<<":(\n";
-	}
 	/*solve*/
 	return 0;
 }
@@ -91,8 +53,5 @@ int main(){
 
 /*think*/
 /*
-dsu[n]裡面，有一些老大
-有幾棵樹，就有幾個老大
-單顆樹裡的所有節點都連向老大
-只需要看兩節點的老大是否相同，就可以知道是不是在同一棵樹上
+
 */
